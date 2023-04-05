f9a067d1b56f410a91724a7165e19a1d2277fda8
Thomas Aleknovic <aleknovic70@gmail.com>
2023-04-05 11:12:10 -0300

    feat: add FindAll and FindByLastname methods

--- 02/spring_hibernate_course/src/main/java/com/thomas/spring_hibernate_course/SpringHibernateCourseApplication.java
+++ 02/spring_hibernate_course/src/main/java/com/thomas/spring_hibernate_course/SpringHibernateCourseApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringHibernateCourseApplication {
 
@@ -18,8 +20,50 @@ public class SpringHibernateCourseApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
 //			createStudent(studentDAO);
-			createMultipleStudents(studentDAO);
+//			createMultipleStudents(studentDAO);
+//			readStudent(studentDAO);
+//			queryForStudents(studentDAO);
+			queryForStudentsByLastname(studentDAO);
 		};
+	}
+
+	private void queryForStudentsByLastname(StudentDAO studentDAO) {
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("James", "Lock", "aleknovic@gmail.com");
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		System.out.println("Searching students by it's lastname...");
+		List<Student> theStudents = studentDAO.findByLastName(tempStudent.getLastName());
+		for (Student student : theStudents){
+			System.out.println(student);
+		}
+	}
+
+	private void queryForStudents(StudentDAO studentDAO) {
+		System.out.println("Searching students...");
+		List<Student> theStudents = studentDAO.findAll();
+
+		for (Student tempStudent : theStudents){
+			System.out.println(tempStudent);
+		}
+
+
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("James", "Aleknovic", "aleknovic@gmail.com");
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+		System.out.println("\n retrieving student with id: " + tempStudent.getId());
+		Student myStudent = studentDAO.findById(tempStudent.getId());
+		System.out.println("Found the student: " + myStudent);
+
+		Student myStudent2 = studentDAO.findById(3);
+		System.out.println("Found the student: " + myStudent2);
 	}
 
 	private void createMultipleStudents(StudentDAO studentDAO) {
--- 02/spring_hibernate_course/src/main/java/com/thomas/spring_hibernate_course/dao/StudentDAO.java
+++ 02/spring_hibernate_course/src/main/java/com/thomas/spring_hibernate_course/dao/StudentDAO.java
@@ -2,7 +2,15 @@ package com.thomas.spring_hibernate_course.dao;
 
 import com.thomas.spring_hibernate_course.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
+
+    Student findById(int id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
 }
